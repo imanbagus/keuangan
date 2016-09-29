03f3635d3e47ff9bd2e4d2dacf868a518ec91ed9
imanbagus <imanelmessiah@gmail.com>
2016-09-29 13:40:36 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -26,15 +26,6 @@ Route::get('input', function()
  return View::make('form');
 });
 
-Route::get('input_data_transaksi', function()
-{
- return View::make('transaksi');
-});
-
-Route::get('laporan', function()
-{
- return View::make('laporan');
-});
 
 Route::get('widget', function()
 {
@@ -46,10 +37,15 @@ Route::get('chart', function()
  return View::make('charts');
 });
 
-//Route::get('form-transaksi', function()
-//{ return View::make('transaksi');});
+Route::get('form-transaksi', function()
+{
+ return View::make('transaksi');
+});
 
-Route::get('ListTransaksi', array('as' => 'show', 'uses' => 'InputTransaksiController@show'));
+Route::get('list-transaksi', function()
+{
+ return View::make('ListTransaksi');
+});
 
 //Route::get('input', array('uses'=> 'InputController@create'));
 
@@ -58,12 +54,40 @@ Route::post('authenticate', 'UserController@authenticate');
 Route::get('logout', 'UserController@logout');
 Route::get('register', 'UserController@register'); 
 Route::post('store', 'UserController@store');
-Route::post('store', 'InputTransaksiController@store');
+
+
+//Laporan
+Route::get('laporan', function(){
+	 $laporan = Laporan::all();
+	 return View::make('listdata')->with('datalaporan', $laporan);
+
+});
+
+
+Route::post('laporan/insert', 'LaporanController@store');
+Route::get('laporan/edit/{id}', 'LaporanController@edit');
+Route::post('laporan/update', 'LaporanController@update');
+Route::get('laporan/delete/{id}', 'LaporanController@delete');
+
+
+//
+
+//transaksi 
+
+
+
+Route::get('ListTransaksi', array('as' => 'show', 'uses' => 'InputTransaksiController@show'));
+Route::post('tambah', 'InputTransaksiController@store');
 Route::get('edit/{id}', array('as' => 'edit', 'uses' => 'InputTransaksiController@edit'));
 Route::put('edit/{id}', array('as' => 'update', 'uses' => 'InputTransaksiController@update'));
 Route::get('delete/{id}', array('as' => 'delete', 'uses' => 'InputTransaksiController@delete'));
 
+Route::get('input_data_transaksi', function()
+{
+ return View::make('transaksi');
+});
 
+//
 //sentry
 
 // Route::get('dashboard', function()
