e2735d41cbdb5b5d9bf094a5baaa614c6e4738c2
imanbagus <imanelmessiah@gmail.com>
2016-09-29 14:16:19 +0700

    Update listdata.blade.php

--- listdata.blade.php
+++ listdata.blade.php
@@ -5,9 +5,13 @@
 
   
 <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">   
+
+  {{ link_to('input', 'Tambah',array ( 'class' => 'btn btn-info custom-btn'))}}
+
  <table class="table">
     <tr>
-       <th>No</th>
+       <th>No </th>
+        <th>No Bukti</th>
        <th>Tanggal</th>
        <th>Keterangan</th>
        <th>No Perkiraan</th>
@@ -15,10 +19,28 @@
        <th>Kredit</th>
        <th>Action</th>
     </tr>
+<?php
+ $i = 1;
+ ?>
+ @foreach($datalaporan as $laporan)
+
    
       <tr>
-     
+         <td>{{ $i }}</td>
+         <td>{{ $laporan->no_bukti }}</td>
+         <td>{{ $laporan->tanggal }}</td>
+         <td>{{ $laporan->keterangan }}</td>
+         <td>{{ $laporan->no_perk }}</td>
+         <td>{{ $laporan->debet}}</td>
+         <td>{{ $laporan->kredit}}</td>
+          <td>{{ link_to_action('LaporanController@edit','Edit', array($laporan->id), array ( 'class' => 'btn btn-warning'))}} | {{ link_to_action('LaporanController@delete', 'Delete', array($laporan->id), array ( 'class' => 'btn btn-danger'))}}</td>
+          
+        
       </tr>
+      <?php
+    $i++;
+    ?>
+    @endforeach
    
  </table>
 </div>
