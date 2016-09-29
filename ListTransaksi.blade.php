e3f4c246a2a85d52946811747b1d5fe9afd8ec6b
Ariq Nugraha <mochariqnugraha@yahoo.co.id>
2016-09-29 12:58:26 +0700

    Update ListTransaksi.blade.php

--- ListTransaksi.blade.php
+++ ListTransaksi.blade.php
@@ -1,10 +1,10 @@
 @extends('layout')
 
 @section('content')
-
-
+     
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main"> 
+{{ link_to('store','Tambah', array ( 'class' => 'btn btn-primary'))}}
   
-<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">   
  <table class="table">
     <tr>
        <th>No</th>
@@ -15,15 +15,18 @@
        <th>Action</th>
     </tr>
    
+    @foreach ($datatransaksi as $data1)
       <tr>
-        @foreach ($datatransaksi as $data1)
+       
           <td>{{ $data1->id}}</td>
           <td>{{ $data1->no_bukti}}</td>
           <td>{{ $data1->tanggal}}</td>
           <td>{{ $data1->nominal }}</td>
           <td>{{ $data1->tipe_transaksi }}</td>
-        @endforeach
+          <td>{{ link_to_action('InputTransaksiController@edit','Edit', array($data1->id), array ( 'class' => 'btn btn-warning'))}} | {{ link_to_action('InputTransaksiController@delete', 'Delete', array($data1->id), array ( 'class' => 'btn btn-danger'))}}</td>
+        
       </tr>
-   
+   @endforeach
  </table>
 </div>
+
